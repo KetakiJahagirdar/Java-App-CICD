27106b71a560da6d96b87c76b617c3f13efd3e0d
Ketaki <ketakijahagirdar@gmail.com>
2026-07-10 16:02:51 +0200

    initial

---
+++ src/main/java/com/example/demo/PaymentController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "UP";
+    }
+
+    @GetMapping("/payment")
+    public String payment() {
+        return "Payment Processed";
+    }
+}
